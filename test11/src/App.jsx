8c5322f2e29272e093675d3c0ec496ad51de17df
ajay4ugit <mamathagaje835@gmail.com>
2024-10-31 13:56:58 +0530

    first commit

---
+++ test11/src/App.jsx
@@ -0,0 +1,11 @@
+import Test from './components/Test'
+
+function App(){
+  return(
+    <div>
+     <Test name="Ajayreddy" role="Trainer"/>
+     <Test salary="150000"/>
+    </div>
+  )
+}
+export default App;
---
+++ test11/src/Test.js
@@ -0,0 +1,9 @@
+function Test(Props){
+    return(
+        <div>
+            Hello {Props.name} your working as {Props.role}
+            My Salary is{Props.salary}
+        </div>
+    )
+}
+export default Test;
